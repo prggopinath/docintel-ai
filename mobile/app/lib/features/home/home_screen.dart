77261ee584778393ee86a1a8b35f7ed238c9b35a
prggopinath <tamiltrendies@gmail.com>
2026-07-25 13:28:46 +0000

    Milestone 2 - homescreen folders

--- mobile/app/lib/features/home/home_screen.dart
+++ mobile/app/lib/features/home/home_screen.dart
@@ -1,26 +1,10 @@
-import 'package:flutter/material.dart';
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-
-      appBar: AppBar(
-        title: const Text("DocIntel AI"),
-      ),
-
-      body: const Center(
-        child: Text(
-          "Home Screen",
-          style: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
-}
+Scaffold(
+  body: Column(
+    children: [
+      WelcomeHeader(),
+      QuickActions(),
+      AIToolsSection(),
+      RecentDocuments(),
+    ],
+  ),
+)
